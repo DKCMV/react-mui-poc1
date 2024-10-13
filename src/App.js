ce226240c7a918b370c69d6683d9b5cb762c972f
Jatin Raghav <Jatin.Raghav@rtctek.global>
2024-10-13 19:14:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Container } from "@mui/material";
+import './App.css';
+import Header  from "./component/Header";
+
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
+
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import {Container, Typography} from '@mui/material';
+import "../App.css";
+
+const Header = () => {
+    return(
+        <Container>
+            <Typography variant="h1" color="primary"> Villen</Typography>
+        </Container>
+    );
+}
+
+export default Header;
